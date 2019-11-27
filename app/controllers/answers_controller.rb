f7752babdf49af3ea351cf363ce7a2c9554d4ac2
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2019-11-27 15:46:08 +0100

    début du quizz

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,27 @@
 class AnswersController < ApplicationController
   def new
+    @question = Question.find(params[:question_id])
+    @options = @question.options
+    @answer = Answer.new
   end
 
   def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    @answer.user = current_user
+    @answer.save!
+
+    if @question.id < 2
+      redirect_to new_question_answer_path(@question.id + 1)
+    else
+      redirect_to root_path
+    end
+    raise
+  end
+
+    private
+
+  def answer_params
+    params.require(:answer).permit(:option_id)
   end
 end
