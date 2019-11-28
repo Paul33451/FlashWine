09545d203026a40fd912e47aebbd951283d79f0a
Paul Manche <paul.manche1@gmail.com>
2019-11-28 17:36:21 +0100

    first version of the dashboard haeder with avatar, name, level and progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,22 @@ class User < ApplicationRecord
   has_many :answers
   has_friendship
   mount_uploader :photo, PhotoUploader
+
+  def level
+    if self.score.nil?
+      return "Bouteille"
+    elsif self.score < 400
+      return "Magnum"
+    elsif self.score < 600
+      return "Jeroboam"
+    elsif self.score < 800
+      return "Impérial"
+    elsif self.score < 1000
+      return "Balthazar"
+    elsif self.score < 1200
+      return "Nabuchodonosor"
+    elsif self.score < 1400
+      return "Melchior"
+    end
+  end
 end
