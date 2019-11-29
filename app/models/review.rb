e3f99ff2c8cba0ce41456013bc2c247806743190
Paul Manche <paul.manche1@gmail.com>
2019-11-29 12:18:59 +0100

    useless validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :wine
   belongs_to :user
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
+
   validates :content, presence: true, length: { minimum: 10 }
 end
