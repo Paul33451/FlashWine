3ac8792000b938ba1b6f503d929de25e9980fa14
Noemie Pierart <noemie.pierart@gmail.com>
2019-12-02 23:32:38 +0100

    Added friends index with search bar + Updated design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where.not(id: current_user.id).where.not(id: current_user.friends.ids)
+    if params[:query].present?
+      first_name_query = "first_name @@ :first_name"
+      last_name_query = "last_name @@ :last_name"
+      email_query = "email @@ :email"
+      @users = @users.where(first_name_query, first_name: "%#{params[:query][:first_name]}%") unless params[:query][:first_name].blank?
+      @users = @users.where(last_name_query, last_name: "%#{params[:query][:last_name]}%") unless params[:query][:last_name].blank?
+      @users = @users.where(email_query, email: "%#{params[:query][:email]}%") unless params[:query][:email].blank?
+    end
   end
 
   def add
