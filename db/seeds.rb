d42208749937596b0c0a309af2a9dd655a1b89bc
Valentin DAUSSE <valentindausse@gmail.com>
2019-12-04 15:08:25 +0100

    add quizz in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -83,12 +83,12 @@ puts "==========================="
 puts "Creating Quiz"
 puts "==========================="
 
-quiz_1 = Quiz.new(title: "Les base de la dégustation", wine: margaux, photo: "quiz1-couleurs.jpg")
-quiz_2 = Quiz.new(title: "Grands crus classés", wine: margaux, photo:"quiz2-crus.jpg")
-quiz_3 = Quiz.new(title: "Accords mets-vins", wine: margaux, photo:"quiz5-mets.jpg")
-quiz_4 = Quiz.new(title: "Terroirs et cépages", wine: margaux, photo:"/quiz3-terroirs.jpg")
-quiz_5 = Quiz.new(title: "Tout sur le bouchon", wine: margaux, photo:"quiz4-bouchons.jpg")
-quiz_6 = Quiz.new(title: "La biodynamie", wine: margaux, photo: "quiz6-biodynamie.jpg")
+quiz_1 = Quiz.new(title: "Les bases de la dégustation", wine: margaux, photo: "quiz1-couleurs.jpg")
+quiz_2 = Quiz.new(title: "Les Régions viticoles en France", wine: margaux, photo:"quiz2-crus.jpg")
+quiz_3 = Quiz.new(title: "Les cépages", wine: margaux, photo:"quiz5-mets.jpg")
+quiz_4 = Quiz.new(title: "Les Grands Crus Classés de Bordeaux", wine: margaux, photo:"/quiz3-terroirs.jpg")
+quiz_5 = Quiz.new(title: "Les appellations de Bordeaux", wine: margaux, photo:"quiz4-bouchons.jpg")
+quiz_6 = Quiz.new(title: "Les appellations de Bourgogne", wine: margaux, photo: "quiz6-biodynamie.jpg")
 
 quiz_1.save!
 quiz_2.save!
@@ -103,7 +103,7 @@ puts "Creating Questions"
 puts "==========================="
 
 question_1 = Question.new(content: "Quelle étape n’existe pas en dégustation ?", position: 1, quiz: quiz_1)
-question_2 = Question.new(content: "Que pouvons-nous examiner lors de visuel olfactif d’un vin?", position: 2, quiz: quiz_1)
+question_2 = Question.new(content: "Que pouvons-nous examiner lors de l'examen d’un vin?", position: 2, quiz: quiz_1)
 question_3 = Question.new(content: "D’où viennent les arômes primaires?", position: 3, quiz: quiz_1)
 question_4 = Question.new(content: " Lequel de ces 4 types d’arômes n’existe pas?", position: 4, quiz: quiz_1)
 question_5 = Question.new(content: "Laquelle de ces impressions peut-on retrouver lors de l’examen gustatif?", position: 5, quiz: quiz_1)
@@ -114,28 +114,68 @@ question_3.save!
 question_4.save!
 question_5.save!
 
-question_6 = Question.new(content: "Question à venir [...] ", position: 1, quiz: quiz_2)
+question_6 = Question.new(content: "Combien y a t-il de grande région viticole en France ?", position: 1, quiz: quiz_2)
+question_7 = Question.new(content: "Quelle région viticole n’existe pas ?", position: 2, quiz: quiz_2)
+question_8 = Question.new(content: "De quelle région viticole provient le Jurançon ?", position: 3, quiz: quiz_2)
+question_9 = Question.new(content: "Dans quelle région est produit le champagne ?", position: 4, quiz: quiz_2)
+question_10 = Question.new(content: "Dans quelle région est produit le Beaujolais nouveau ?", position: 5, quiz: quiz_2)
 question_6.save!
-
-question_7 = Question.new(content: "Question à venir [...]", position: 1, quiz: quiz_3)
 question_7.save!
-
-question_8 = Question.new(content: "Question à venir [...]", position: 1, quiz: quiz_4)
 question_8.save!
-
-question_9 = Question.new(content: "Question à venir [...]", position: 1, quiz: quiz_5)
 question_9.save!
-
-question_10 = Question.new(content: "Question à venir [...]", position: 1, quiz: quiz_5)
 question_10.save!
 
-question_11 = Question.new(content: "Question à venir [...]", position: 1, quiz: quiz_6)
+question_11 = Question.new(content: "Qu’est ce qu’un cépage ?", position: 1, quiz: quiz_3)
+question_12 = Question.new(content: "Combien y a t-il environ de cépages cultivés dans le monde ?", position: 2, quiz: quiz_3)
+question_13 = Question.new(content: "Quel cépage est principalement utilisé en Bourgogne ?", position: 3, quiz: quiz_3)
+question_14 = Question.new(content: "Quel cépage n’est pas utilisé dans la confection d’un champagne ?", position: 4, quiz: quiz_3)
+question_15 = Question.new(content: "Lequel de ces cépages n’est pas un cépage rouge ?", position: 5, quiz: quiz_3)
 question_11.save!
+question_12.save!
+question_13.save!
+question_14.save!
+question_15.save!
+
+question_16 = Question.new(content: "Comment s’appelle le classement officiel des vins de Bordeaux ?", position: 1, quiz: quiz_4)
+question_17 = Question.new(content: "Qui a ordonné  le classement de 1855 ?", position: 2, quiz: quiz_4)
+question_18 = Question.new(content: "Quel vin n’est pas un cru classé ?", position: 3, quiz: quiz_4)
+question_19 = Question.new(content: "Combien de changement a connu le classement officiel depuis sa créaton ?", position: 4, quiz: quiz_4)
+question_20 = Question.new(content: "Quel segment du classement n’existe pas ?", position: 5, quiz: quiz_4)
+question_16.save!
+question_17.save!
+question_18.save!
+question_19.save!
+question_20.save!
+
+question_21 = Question.new(content: "Laquelle de ces 4 appellations n’appartient pas à la région bordelaise ?", position: 1, quiz: quiz_5)
+question_22 = Question.new(content: "Laquelle de ces 4 appellations appartient à la sous-région “Médoc” ?", position: 2, quiz: quiz_5)
+question_23 = Question.new(content: "Laquelle de ces 4 appellations appartient à la sous-région “Graves” ?", position: 3, quiz: quiz_5)
+question_24 = Question.new(content: "Laquelle de ces 4 appellations appartient à la sous-région “Libournais” ?", position: 4, quiz: quiz_5)
+question_25 = Question.new(content: "Quels cépages peut-on retrouver dans les vins de Bordeaux ?", position: 5, quiz: quiz_5)
+question_21.save!
+question_22.save!
+question_23.save!
+question_24.save!
+question_25.save!
+
+question_26 = Question.new(content: "Laquelle de ces 4 appellations n’appartient pas à la région bordelaise ?", position: 1, quiz: quiz_6)
+question_27 = Question.new(content: "Laquelle de ces 4 appellations appartient à la sous-région “Médoc” ?", position: 2, quiz: quiz_6)
+question_28 = Question.new(content: "Laquelle de ces 4 appellations appartient à la sous-région “Graves” ?", position: 3, quiz: quiz_6)
+question_29 = Question.new(content: "Laquelle de ces 4 appellations appartient à la sous-région “Libournais” ?", position: 4, quiz: quiz_6)
+question_30 = Question.new(content: "Quels cépages peut-on retrouver dans les vins de Bordeaux ?", position: 5, quiz: quiz_6)
+question_26.save!
+question_27.save!
+question_28.save!
+question_29.save!
+question_30.save!
+
 
 # Options
 puts "==========================="
 puts "Creating Options"
 puts "==========================="
+
+#######Options for quiz 1 :
 
 #Options for question 1 :
 option_1 = Option.new(title: "L’examen visuel", right: false, question: question_1)
@@ -167,6 +207,43 @@ option_18 = Option.new(title: "L’attaque", right: true, question: question_5)
 option_19 = Option.new(title: "La demi-finale", right: false, question: question_5)
 option_20 = Option.new(title: "La contre attaque", right: false, question: question_5)
 
+#######Options for quiz 2 :
+
+#Options for question 1 :
+option_21 = Option.new(title: "3", right: false, question: question_6)
+option_22 = Option.new(title: "10", right: false, question: question_6)
+option_23 = Option.new(title: "13", right: true, question: question_6)
+option_24 = Option.new(title: "15", right: false, question: question_6)
+
+#Options for question 2 :
+option_25 = Option.new(title: "Bordeaux", right: false, question: question_7)
+option_26 = Option.new(title: "Savoie", right: false, question: question_7)
+option_27 = Option.new(title: "Villeneuvois", right: true, question: question_7)
+option_28 = Option.new(title: "Corse", right: false, question: question_7)
+
+#Options for question 3 :
+option_29 = Option.new(title: "Le Jura", right: false, question: question_8)
+option_30 = Option.new(title: "La Savoie", right: false, question: question_8)
+option_31 = Option.new(title: "Le Sud-Ouest", right: true, question: question_8)
+option_32 = Option.new(title: "Le Languedoc-Roussillon", right: false, question: question_8)
+
+#Options for question 4 :
+option_33 = Option.new(title: "Alsace", right: false, question: question_9)
+option_34 = Option.new(title: "Champagne", right: true, question: question_9)
+option_35 = Option.new(title: "Bordeaux", right: false, question: question_9)
+option_36 = Option.new(title: "Provence", right: false, question: question_9)
+
+#Options for question 5 :
+option_37 = Option.new(title: "Bourgogne", right: false, question: question_10)
+option_38 = Option.new(title: "Beaujolais", right: true, question: question_10)
+option_39 = Option.new(title: "Bordeaux", right: false, question: question_10)
+option_40 = Option.new(title: "Corse", right: false, question: question_10)
+
+
+#######Options for quiz 3 :
+
+
+
 option_1.save!
 option_2.save!
 option_3.save!
@@ -189,10 +266,10 @@ option_19.save!
 option_20.save!
 
 #Options for question 6 :
-option_21 = Option.new(title: "Parce que ...", right: false, question: question_6)
-option_22 = Option.new(title: "A cause du raisin blanc", right: true, question: question_6)
-option_23 = Option.new(title: "A cause du sulfate", right: false, question: question_6)
-option_24 = Option.new(title: "Parce qu'il est plus sucré", right: false, question: question_6)
+option_21 = Option.new(title: "3", right: false, question: question_6)
+option_22 = Option.new(title: "10", right: true, question: question_6)
+option_23 = Option.new(title: "13", right: false, question: question_6)
+option_24 = Option.new(title: "15", right: false, question: question_6)
 
 option_21.save!
 option_22.save!
@@ -200,10 +277,10 @@ option_23.save!
 option_24.save!
 
 #Options for question 7 :
-option_25 = Option.new(title: "Réponse 1 [...]", right: false, question: question_7)
-option_26 = Option.new(title: "Réponse 2 [...]", right: false, question: question_7)
-option_27 = Option.new(title: "Réponse 3 [...]", right: false, question: question_7)
-option_28 = Option.new(title: "Réponse 4 [...]", right: true, question: question_7)
+option_25 = Option.new(title: "Le Bordeaux", right: false, question: question_7)
+option_26 = Option.new(title: "La Savoie", right: false, question: question_7)
+option_27 = Option.new(title: "Le Villeneuvois", right: false, question: question_7)
+option_28 = Option.new(title: "La Corse", right: true, question: question_7)
 
 option_25.save!
 option_26.save!
@@ -211,10 +288,10 @@ option_27.save!
 option_28.save!
 
 #Options for question 8 :
-option_29 = Option.new(title: "Réponse 1 [...]", right: false, question: question_7)
-option_30 = Option.new(title: "Réponse 2 [...]", right: false, question: question_7)
-option_31 = Option.new(title: "Réponse 3 [...]", right: false, question: question_7)
-option_32 = Option.new(title: "Réponse 4 [...]", right: true, question: question_7)
+option_29 = Option.new(title: "Le Jura", right: false, question: question_8)
+option_30 = Option.new(title: "La Savoie", right: false, question: question_8)
+option_31 = Option.new(title: "Le Sud-Ouest", right: false, question: question_8)
+option_32 = Option.new(title: "Le Languedoc-Roussillon", right: true, question: question_8)
 
 option_29.save!
 option_30.save!
@@ -222,10 +299,10 @@ option_31.save!
 option_32.save!
 
 #Options for question 9 :
-option_33 = Option.new(title: "Réponse 1 [...]", right: false, question: question_9)
-option_34 = Option.new(title: "Réponse 2 [...]", right: false, question: question_9)
-option_35 = Option.new(title: "Réponse 3 [...]", right: false, question: question_9)
-option_36 = Option.new(title: "Réponse 4 [...]", right: true, question: question_9)
+option_33 = Option.new(title: "Alsace", right: false, question: question_9)
+option_34 = Option.new(title: "Champagne", right: false, question: question_9)
+option_35 = Option.new(title: "Bordeaux", right: false, question: question_9)
+option_36 = Option.new(title: "Provence", right: true, question: question_9)
 
 option_33.save!
 option_34.save!
@@ -233,10 +310,10 @@ option_35.save!
 option_36.save!
 
 #Options for question 10 :
-option_37 = Option.new(title: "Réponse 1 [...]", right: false, question: question_10)
-option_38 = Option.new(title: "Réponse 2 [...]", right: false, question: question_10)
-option_39 = Option.new(title: "Réponse 3 [...]", right: false, question: question_10)
-option_40 = Option.new(title: "Réponse 4 [...]", right: true, question: question_10)
+option_37 = Option.new(title: "Bourgogne", right: false, question: question_10)
+option_38 = Option.new(title: "Beaujolais", right: false, question: question_10)
+option_39 = Option.new(title: "Bordeaux", right: false, question: question_10)
+option_40 = Option.new(title: "Corse", right: true, question: question_10)
 
 option_37.save!
 option_38.save!
@@ -244,15 +321,55 @@ option_39.save!
 option_40.save!
 
 #Options for question 11 :
-option_41 = Option.new(title: "Réponse 1 [...]", right: false, question: question_11)
-option_42 = Option.new(title: "Réponse 2 [...]", right: false, question: question_11)
-option_43 = Option.new(title: "Réponse 3 [...]", right: false, question: question_11)
-option_44 = Option.new(title: "Réponse 4 [...]", right: true, question: question_11)
+option_41 = Option.new(title: "Une manière de faire le vin", right: false, question: question_11)
+option_42 = Option.new(title: "Le fruit d’un type de vigne spécifique", right: false, question: question_11)
+option_43 = Option.new(title: "Une AOC", right: false, question: question_11)
+option_44 = Option.new(title: "Un type de vin", right: true, question: question_11)
 
 option_41.save!
 option_42.save!
 option_43.save!
 option_44.save!
+
+option_45 = Option.new(title: "50", right: false, question: question_12)
+option_46 = Option.new(title: "250", right: false, question: question_12)
+option_47 = Option.new(title: "1000", right: false, question: question_12)
+option_48 = Option.new(title: "5000", right: true, question: question_12)
+
+option_45.save!
+option_46.save!
+option_47.save!
+option_48.save!
+
+option_49 = Option.new(title: "Le pinot noir", right: false, question: question_13)
+option_50 = Option.new(title: "Le pinot", right: false, question: question_13)
+option_51 = Option.new(title: "Le pinocchio", right: false, question: question_13)
+option_52 = Option.new(title: "Le cabernet sauvignon", right: true, question: question_13)
+
+option_49.save!
+option_50.save!
+option_51.save!
+option_52.save!
+
+option_53 = Option.new(title: "Le pinot meunier", right: false, question: question_14)
+option_54 = Option.new(title: "Le merlot", right: false, question: question_14)
+option_55 = Option.new(title: "Le Chardonnay", right: false, question: question_14)
+option_56 = Option.new(title: "Le pinot noir", right: true, question: question_14)
+
+option_53.save!
+option_54.save!
+option_55.save!
+option_56.save!
+
+option_57 = Option.new(title: "Le Cabernet Sauvignon", right: false, question: question_15)
+option_58 = Option.new(title: "Le Merlot", right: false, question: question_15)
+option_59 = Option.new(title: "Le Chardonnay", right: false, question: question_15)
+option_60 = Option.new(title: "La Syrah", right: true, question: question_15)
+
+option_57.save!
+option_58.save!
+option_59.save!
+option_60.save!
 
 # puts "==========================="
 # puts "Creating recommendations"
