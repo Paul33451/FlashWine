1b2fdc2793bc7f0de379e24af08be93682a1345b
Paul Manche <paul.manche1@gmail.com>
2019-12-05 17:24:12 +0100

    redirect to the right point

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_124353) do
+ActiveRecord::Schema.define(version: 2019_12_05_162104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_124353) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.integer "requirement"
+    t.string "description"
   end
 
   create_table "options", force: :cascade do |t|
