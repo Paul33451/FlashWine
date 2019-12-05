f3be9914eadcde7805d9196f238f7fb761ef2091
Paul Manche <paul.manche1@gmail.com>
2019-12-05 18:07:45 +0100

    review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,4 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
 
-  validates :content, presence: true, length: { minimum: 10 }
 end
